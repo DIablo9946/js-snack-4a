c0d9355609d00da74f1ab0b0476edd3554b5eb84
Diablo9946 <diabloch9946@gmail.com>
2019-08-22 02:06:39 +0200

    Better measure,

--- action.js
+++ action.js
@@ -5,10 +5,21 @@
 var arr1 = ["Cane1", "Cane2", "Cane3", "Cane4", "Cane5"];
 var arr2 = ["Gatto1", "Gatto2"];
 
-for (var i = 0; i < arr1.length; i++) {
-  if (arr1.length > arr2.length) {
-    arr2.push("nuovoGatto");
-  }
+if (arr1.length == arr2.length){
+  console.log("Non faccio nulla");
+} else if (arr1.length > arr2.length){
+  var maxArr = arr1;
+  var minArr = arr2;
+} else {
+  var maxArr = arr2;
+  var minArr = arr1;
+}
+
+var diff = maxArr.length - minArr.length;
+
+for (var i = 0; i < diff; i++) {
+    var nuovogatto = prompt("Inserisci il nuovo numero con Gatto")
+    minArr.push(nuovogatto);
 };
 
 console.log(arr1, arr2);
